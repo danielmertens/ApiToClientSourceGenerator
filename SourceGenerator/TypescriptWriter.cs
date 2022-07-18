a64f4d992bf3f0fcff5687a35fe5d49d440f71be
Daniel <danielmertensfaema@hotmail.com>
2022-07-19 01:36:49 +0200

    Initial commit

---
+++ SourceGenerator/TypescriptWriter.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace SourceGenerator
+{
+    public static class TypescriptWriter
+    {
+        public static string Write(List<ApiDefinition> apiList, List<ClassDefinition> classList)
+        {
+            var text = "";
+            text += CreateApiCalls(apiList);
+            text += CreateClassTypes(classList);
+
+            return text;
+        }
+
+        private static string CreateApiCalls(List<ApiDefinition> apiList)
+        {
+            var builder = new StringBuilder();
+
+            foreach (var item in apiList)
+            {
+                builder.AppendLine($"export const fetch{item.ControllerName}{item.MethodName}: () => Promise<{item.ReturnTypeString}> = async () => {{");
+                builder.AppendLine($"  const response = await fetch(\"{item.Url}\");");
+                builder.AppendLine($"  const body = await response.json();");
+                builder.AppendLine($"  return body;");
+                builder.AppendLine("}");
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+
+        private static string CreateClassTypes(List<ClassDefinition> classList)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var item in classList)
+            {
+                builder.AppendLine($"export type {item.ClassName} = {{");
+
+                foreach (var propDef in item.Properties)
+                {
+                    builder.AppendLine($"  {LowerFirstLetter(propDef.name)}: {TypeToTypescriptType(propDef.type)}");
+                }
+
+                builder.AppendLine("}");
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+
+
+        private static string LowerFirstLetter(string name)
+        {
+            return name.Substring(0, 1).ToLowerInvariant() + name.Substring(1);
+        }
+
+        private static string TypeToTypescriptType(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    return "number";
+                case "string":
+                    return "string";
+                case "DateTime":
+                    return "Date";
+
+                default: return "any";
+            }
+        }
+    }
+}
---
+++ WebApplication3/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 4).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "Test")]
+        public WeatherForecast Test()
+        {
+            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(Random.Shared.Next(1, 5)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
+        }
+    }
+}
